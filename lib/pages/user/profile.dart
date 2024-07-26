3e4e01003d03772363e1f656a5558bc25fd445eb
sohebalam <sohebalam@hotmail.com>
2024-07-26 21:34:20 +0100

    edit and delete

--- lib/pages/user/profile.dart
+++ lib/pages/user/profile.dart
@@ -5,7 +5,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:travelist/services/shared_functions.dart';
-import '../../services/widgets/image_picker.dart'; // Adjust the import according to your project structure
+import 'package:travelist/models/user_model.dart';
+import 'package:travelist/services/widgets/image_picker.dart'; // Adjust the import according to your project structure
 
 class UserProfilePage extends StatefulWidget {
   @override
@@ -20,12 +21,42 @@ class _UserProfilePageState extends State<UserProfilePage> {
   final TextEditingController _interestController = TextEditingController();
   File? _image;
   late Future<DocumentSnapshot> _userFuture;
+  bool isAdmin = false;
+  List<UserModel> allUsers = [];
+  String selectedView = 'Interests'; // Default view
+  String? editingInterest; // To keep track of the interest being edited
 
   @override
   void initState() {
     super.initState();
     _userFuture =
         FirebaseFirestore.instance.collection('users').doc(user?.uid).get();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    var userDoc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user?.uid)
+        .get();
+    var userData = userDoc.data() as Map<String, dynamic>;
+    setState(() {
+      isAdmin = userData['isAdmin'] ?? false;
+    });
+
+    if (isAdmin) {
+      await _loadAllUsers();
+    }
+  }
+
+  Future<void> _loadAllUsers() async {
+    var querySnapshot =
+        await FirebaseFirestore.instance.collection('users').get();
+    setState(() {
+      allUsers = querySnapshot.docs
+          .map((doc) => UserModel.fromJson(doc.data()))
+          .toList();
+    });
   }
 
   Future<void> _updateProfile() async {
@@ -84,6 +115,53 @@ class _UserProfilePageState extends State<UserProfilePage> {
     }
   }
 
+  Future<void> _editInterest(String oldInterest, String newInterest) async {
+    if (newInterest.trim().isEmpty) return;
+    try {
+      final userRef =
+          FirebaseFirestore.instance.collection('users').doc(user!.uid);
+      final userDoc = await userRef.get();
+      List<String> interests = List<String>.from(userDoc['interests']);
+      interests[interests.indexOf(oldInterest)] =
+          newInterest.trim().toLowerCase();
+      await userRef.update({'interests': interests});
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Interest updated')),
+      );
+      setState(() {
+        editingInterest = null;
+        _userFuture = userRef.get();
+      });
+    } catch (e) {
+      print('Error editing interest: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to edit interest')),
+      );
+    }
+  }
+
+  Future<void> _deleteInterest(String interest) async {
+    try {
+      final userRef =
+          FirebaseFirestore.instance.collection('users').doc(user!.uid);
+      final userDoc = await userRef.get();
+      List<String> interests = List<String>.from(userDoc['interests']);
+      interests.remove(interest);
+      await userRef.update({'interests': interests});
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Interest deleted')),
+      );
+      setState(() {
+        _userFuture = userRef.get();
+      });
+    } catch (e) {
+      print('Error deleting interest: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to delete interest')),
+      );
+    }
+  }
+
   Future<void> _pickImage() async {
     final pickedImage = await pickImage(context);
     if (pickedImage != null) {
@@ -123,20 +201,36 @@ class _UserProfilePageState extends State<UserProfilePage> {
             _imageController.text = data['image'];
           }
 
+          List<String> interests = data['interests']?.cast<String>() ?? [];
+
           return SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  if (isAdmin)
+                    DropdownButton<String>(
+                      value: selectedView,
+                      items: ['Interests', 'Users'].map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedView = newValue!;
+                        });
+                      },
+                    ),
                   Stack(
                     children: [
                       CircleAvatar(
                         radius: 50,
                         backgroundImage: _image != null
                             ? FileImage(_image!)
-                            : NetworkImage(data['image'] ?? 'assets/person.png')
-                                as ImageProvider,
+                            : NetworkImage(_imageController.text),
                         onBackgroundImageError: (_, __) {
                           setState(() {
                             _image = null;
@@ -185,47 +279,127 @@ class _UserProfilePageState extends State<UserProfilePage> {
                   SizedBox(height: 16),
                   Divider(),
                   SizedBox(height: 16),
-                  Text(
-                    'Interests',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 8),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: data['interests'].length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: ListTile(
-                          title: Text(data['interests'][index]),
-                          leading: Icon(Icons.star, color: Colors.teal),
-                        ),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 16),
-                  TextField(
-                    controller: _interestController,
-                    decoration: InputDecoration(
-                      labelText: 'Add Interest',
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.add),
+                  if (selectedView == 'Interests') ...[
+                    Text(
+                      'Interests',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  ElevatedButton(
-                    onPressed: _addInterest,
-                    child: Text('Add Interest'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.teal,
+                    SizedBox(height: 8),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: interests.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            title: editingInterest == interests[index]
+                                ? TextField(
+                                    controller: _interestController
+                                      ..text = interests[index],
+                                    onSubmitted: (newValue) {
+                                      _editInterest(interests[index], newValue);
+                                    },
+                                  )
+                                : Text(interests[index]),
+                            leading: Icon(Icons.star, color: Colors.teal),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.edit, color: Colors.blue),
+                                  onPressed: () {
+                                    setState(() {
+                                      editingInterest = interests[index];
+                                      _interestController.text =
+                                          interests[index];
+                                    });
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.delete, color: Colors.red),
+                                  onPressed: () {
+                                    _showDeleteConfirmationDialog(
+                                        interests[index]);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  ),
+                    SizedBox(height: 16),
+                    TextField(
+                      controller: _interestController,
+                      decoration: InputDecoration(
+                        labelText: 'Add Interest',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.add),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    ElevatedButton(
+                      onPressed: _addInterest,
+                      child: Text('Add Interest'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.teal,
+                      ),
+                    ),
+                  ] else ...[
+                    Text(
+                      'Users',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: allUsers.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            title: Text(allUsers[index].email),
+                            leading: Icon(Icons.person, color: Colors.teal),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
                 ],
               ),
             ),
           );
         },
       ),
+    );
+  }
+
+  Future<void> _showDeleteConfirmationDialog(String interest) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Delete Interest'),
+          content: Text('Are you sure you want to delete this interest?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _deleteInterest(interest);
+                Navigator.of(context).pop();
+              },
+              child: Text('Delete'),
+            ),
+          ],
+        );
+      },
     );
   }
 
